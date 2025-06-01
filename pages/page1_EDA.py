cd4d58852b3208d5bf6d6751513f4c3b15387770
hay013 <haymatt013@gmail.com>
2025-06-01 11:42:14 +0700

    Update page1_EDA.py

--- pages/page1_EDA.py
+++ pages/page1_EDA.py
@@ -1,1 +1,27 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from utils.model_utils import load_data
+
+def run():
+    st.title("Exploratory Data Analysis")
+    df = load_data()
+
+    st.subheader("Tampilkan Dataset")
+    st.dataframe(df.head())
+
+    st.subheader("Karakteristik Data")
+    st.write(df.describe())
+
+    st.subheader("Visualisasi Data")
+    st.write("Distribusi Stroke:")
+    fig, ax = plt.subplots()
+    sns.countplot(x='stroke', data=df, ax=ax)
+    st.pyplot(fig)
+
+    st.write("Korelasi Antar Fitur:")
+    fig, ax = plt.subplots()
+    sns.heatmap(df.corr(), annot=True, cmap="coolwarm", ax=ax)
+    st.pyplot(fig)
 
