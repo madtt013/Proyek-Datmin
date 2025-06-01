fb4943634f7e6aeb730134e8ac635010688c3b52
hay013 <haymatt013@gmail.com>
2025-06-01 13:01:44 +0700

    Update model.py

--- pages/model.py
+++ pages/model.py
@@ -1,17 +1,7 @@
-import streamlit as st
 from utils.data_loader import load_data
-from utils.model_utils import train_model
-import json
 
-st.title("📊 Pelatihan Model")
-
+# Memuat dataset
 df = load_data()
 
-if st.button("Train Model"):
-    report = train_model(df)
-    st.write("Model Performance:")
-    st.json(report)
-    
-st.success("Model dilatih!")
-st.subheader("Classification Report")
-st.json(report)
+# Menampilkan nama kolom
+print(df.columns)
